fa3d073b9787c5d494c92b7149c53f71a9630414
bmcgrane302 <bmcgrane302@yahoo.com>
2018-01-15 12:36:10 -0700

    added firebase auth

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyAO0n3lB7jxKNTG1phuc1SNoP9X_7_sV4s',
+    authDomain: 'manager-25293.firebaseapp.com',
+    databaseURL: 'https://manager-25293.firebaseio.com',
+    projectId: 'manager-25293',
+    storageBucket: 'manager-25293.appspot.com',
+    messagingSenderId: '191027454434'
+  };
+  firebase.initializeApp(config);
+  }
+
+  render () {
+    return (
+
+      <Provider store={createStore(reducers)}>
+       <View>
+        <Text>
+          Hello
+        </Text>
+       </View>
+      </Provider>
+    )
+  }
+}
+
+export default App;
