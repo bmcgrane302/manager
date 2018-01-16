2f9f4ecc167b28680adc4138116d4678c2fe880c
bmcgrane302 <bmcgrane302@yahoo.com>
2018-01-16 16:06:01 -0700

    connected database

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
   EMPLOYEE_UPDATE
 } from './types';
@@ -6,5 +9,15 @@ export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+
+ return () => {
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift })
+    .then(() => Actions.pop());
   };
 };
