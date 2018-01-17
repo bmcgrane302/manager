e1f68bcf3c00b3ecba95f43f7cc25fc45b4cdc07
bmcgrane302 <bmcgrane302@yahoo.com>
2018-01-16 17:22:23 -0700

    create user

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,9 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -15,9 +17,23 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
- return () => {
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift })
-    .then(() => Actions.pop());
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.main({ type: 'reset' });
+      });
+  };
+};
+
+export const employeesFetch = () => {
+ const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+     .on('value', snapshot => {
+       dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val()});
+     });
   };
 };
